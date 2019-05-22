0ac99de54cf2d92e539aa017fd80e3790da0fbc7
Josemaria <josemarianriagu25@gmail.com>
2019-05-22 12:04:36 +0800

    Create data variables for team members

---
+++ js/variables.js
@@ -0,0 +1,7 @@
+// define member data
+const teamData = [{src: "/img/jonsimmons.jpg",alt: "portrait of jon simmons",heading3: "Jon Simmons",paragraph: "Team Lead"},{src: "/img/lennoxprincejr.jpg",alt: "portrait of lennox prince",heading3: "Lennox Prince Jr.",paragraph: "UX Designer"},{src: "/img/benjaminchandler.jpg",alt: "portrait of benjamin chandler",heading3: "Benjamin Chandler",paragraph: "Backend Developer"},{src: "/img/douglasbowen.jpg",alt: "portrait of douglas bowen",heading3: "Douglas Bowen",paragraph: "Frontend Developer"},{src: "/img/peishanlee.jpg",alt: "portrait of peishan lee",heading3: "Peishan (Penny) Lee",paragraph: "Web UI Developer"},{src: "/img/josemarianriagu.jpg",alt: "portrait of josemaria nriagu",heading3: "Josemaria Nriagu",paragraph: "Web UI Developer"},]
+
+// get parent container
+const teambox = document.querySelector('.team-box');
+
+// ********************************************************************************************************************
