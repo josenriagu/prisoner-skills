b81adf7b5e84837e24c34a25cd21ed660efabca7
Josemaria <josemarianriagu25@gmail.com>
2019-05-22 12:37:12 +0800

    Create features using class constructors

--- js/index.js
+++ js/index.js
@@ -1,10 +1,60 @@
+// function to create and append each feature
+function doFeatureBuild() {
+    const feature = features.map(feature => {
+        return new FeatureBuilder(feature)
+    });
+    feature.forEach(instance => {
+        featuresbox.appendChild(instance.article)
+    })
+}
+
+// features class constructor
+class FeatureBuilder {
+    constructor(feature) {
+        this.feature = feature;
+        this.article = document.createElement('article');
+        this.div = document.createElement('div');
+        this.img = document.createElement('img');
+        this.heading2 = document.createElement('h2');
+        this.paragraph = document.createElement('p');
+
+        //  call the associated methods sequentially
+        this.constructArticle();
+        this.constructDiv();
+        this.constructHeading();
+        this.constructParagraph();
+    }
+    constructArticle() {
+        this.article.classList.add('features');
+    }
+    constructDiv() {
+        this.div.classList.add('img-box');
+        this.img.src = this.feature.src;
+        this.img.alt = this.feature.alt;
+        this.div.appendChild(this.img);
+        this.article.appendChild(this.div);
+    }
+    constructHeading() {
+        this.heading2.textContent = this.feature.heading2;
+        this.article.appendChild(this.heading2);
+    }
+    constructParagraph() {
+        this.paragraph.textContent = this.feature.paragraph;
+        this.article.appendChild(this.paragraph);
+    }
+}
+
+doFeatureBuild();
+
+// ********************************************************************************************************************
+
 // function to create and append each team member
 function doTeamBuild() {
     const member = teamData.map(member => {
         return new TeamBuilder(member)
     });
     member.forEach(instance => {
-        teambox.appendChild(instance.article)
+        teambox.appendChild(instance.article);
     })
 }
 
